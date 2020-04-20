f13f9ead745c03332d15f103c57bfa2dd13dab5d
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-19 20:57:47 -0400

    Added additional test cases

--- tests/spec.js
+++ tests/spec.js
@@ -12,7 +12,7 @@ describe("Calculator()", function () {
   })
 
   it("should subtract 2-2", function () {
-    expect(calculate("2-2")).to.equal(4)
+    expect(calculate("2-2")).to.equal(0)
   })
 
   it("should multiply 2*2", function () {
@@ -20,6 +20,38 @@ describe("Calculator()", function () {
   })
 
   it("should divide 2/2", function () {
-    expect(calculate("2/2")).to.equal(4)
+    expect(calculate("2/2")).to.equal(1)
+  })
+
+  it("should add 37+37", function () {
+    expect(calculate("37+37")).to.equal(74)
+  })
+
+  it("should subtract 37-37", function () {
+    expect(calculate("37-37")).to.equal(0)
+  })
+
+  it("should multiply 37×37", function () {
+    expect(calculate("37×37")).to.equal(1369)
+  })
+
+  it("should divide 37÷37", function () {
+    expect(calculate("37÷37")).to.equal(1)
+  })
+
+  it("should accept multiple additions and subtractions 37+37-37-37", function () {
+    expect(calculate("37+37-37-37")).to.equal(-74)
+  })
+
+  it("should accept multiple multiplications and divisions 37÷37×37*37/37×37", function () {
+    expect(calculate("37÷37×37*37/37×37")).to.equal(1369)
+  })
+
+  it("should handle order of operations with division 37-37+37÷37", function () {
+    expect(calculate("37-37+37÷37")).to.equal(1)
+  })
+
+  it("should handle order of operations with multiplication 37+37-37×37", function () {
+    expect(calculate("37-37+37÷37")).to.equal(1)
   })
 })
