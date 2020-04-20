4afeab52f85c9a73633829b55f1ffaeb1f36332a
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-19 21:03:33 -0400

    Began implementing for loop over operators

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,40 +1,55 @@
 const Calculator = () => {
-  return (inputString) => {
+  returnValue = (inputString) => {
     // Operators are always a single character, so we can split the display
     // string by the operators to get an array of numbers, which we coerce:
-    const parsedNumbers = inputString.split(/\+|\-|\×|\÷|\*|\//g).map((number) => {
-      const parsedNumber = Number(number)
-      Number.isNaN(parsedNumber)
-        ? console.log(`"${number}" could not be parsed and was stored as NaN`)
-        : false
-      return parsedNumber
-    })
+    const parsedNumbers = inputString
+      .split(/\+|\-|\×|\÷|\*|\//g)
+      .map((number) => {
+        const parsedNumber = Number(number)
+        Number.isNaN(parsedNumber)
+          ? console.log(`"${number}" could not be parsed and was stored as NaN`)
+          : false
+        returnValue = parsedNumber
+      })
     // Because numbers can be multiple digits, integers or with a decimal
     // point, first we remove all the numbers, then we split the string:
     const parsedOperators = inputString.replace(/[0-9]|\./g, "").split("")
 
-    const operator = parsedOperators[0]
+    const returnValue = 0
 
-    switch (operator) {
-      case "+":
-        return parsedNumbers[0] + parsedNumbers[1]
-        break
-      case "-":
-        return parsedNumbers[0] - parsedNumbers[1]
-        break
-      case "*":
-      case "×":
-        return parsedNumbers[0] * parsedNumbers[1]
-        break
-      case "/":
-      case "÷":
-        return parsedNumbers[0] / parsedNumbers[1]
-        break
-      default:
-        console.log(`Unknown operator "${operator}"`)
+    for (
+      let numbersIndex = 0;
+      numbersIndex < parsedNumbers.length;
+      numbersIndex += 2
+    ) {
+      const operator = parsedOperators[numbersIndex]
+
+      switch (operator) {
+        case "+":
+          returnValue +=
+            parsedNumbers[numbersIndex] + parsedNumbers[numbersIndex + 1]
+          break
+        case "-":
+          returnValue +=
+            parsedNumbers[numbersIndex] - parsedNumbers[numbersIndex + 1]
+          break
+        case "*":
+        case "×":
+          returnValue +=
+            parsedNumbers[numbersIndex] * parsedNumbers[numbersIndex + 1]
+          break
+        case "/":
+        case "÷":
+          returnValue +=
+            parsedNumbers[numbersIndex] / parsedNumbers[numbersIndex + 1]
+          break
+        default:
+          console.log(`Unknown operator "${operator}"`)
+          returnValue = NaN
+      }
     }
 
-    return Number(inputString)
+    return returnValue
   }
 }
 
