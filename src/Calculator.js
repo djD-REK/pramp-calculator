4b0cac60f197b191e885717fbe46ac727829f9a8
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-04-19 20:34:19 -0400

    Began implementation of number parser algorithm (calculate)

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,29 @@
 const Calculator = () => {
-  return {
-    calculate: (inputString) => {
-      const parsedNumbers = inputString
-      const parsedOperators = inputString
-      return Number(inputString)
+  return (inputString) => {
+    // Operators are always a single character, so we can split the display
+    // string by the operators to get an array of numbers, which we coerce:
+    const parsedNumbers = inputString.split(/\+|\-|\×|\÷/g).map((number) => {
+      const parsedNumber = Number(number)
+      Number.isNan(parsedNumber)
+        ? console.log(`"${number}" could not be parsed and was stored as NaN`)
+        : false
+      return parsedNumber
+    })
+    // Because numbers can be multiple digits, integers or with a decimal
+    // point, first we remove all the numbers, then we split the string:
+    const parsedOperators = inputString.replace(/[0-9]|\./g, "").split("")
+
+    const operator = parsedOperators[0]
+
+    switch (operator) {
+      case "+":
+        return parsedNumbers[0] + parsedNumbers[1]
+        break
+      default:
+        console.log(`Unknown operator "${operator}"`)
     }
+
+    return Number(inputString)
   }
 }
 
